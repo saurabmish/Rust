0390313a4934adae412e480cce5e65b2287e7e3e
Saurabh Mishra <m.saura8h@gmail.com>
2020-04-17 22:29:18 -0700

    removed target directory from project

--- hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae.d
+++
@@ -1,5 +0,0 @@
-/Users/saura8h/Desktop/rust-lang/projects/hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae: src/main.rs
-
-/Users/saura8h/Desktop/rust-lang/projects/hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/saura8h/Desktop/rust-lang/projects/hello_cargo/target/debug/hello_cargo: /Users/saura8h/Desktop/rust-lang/projects/hello_cargo/src/main.rs
